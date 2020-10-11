7ab3cd098795f6e2b187e9076685e661c4fb1ccc
heather.haylett <heatherhaylett@gmail.com>
2020-10-11 14:22:25 -0500

    (feat) start click events

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+// const changePage = (pageName) => {
+//     window.location.href = `${pageName}.html`;
+// }
+
+// const clickEvent = (pageName) => {
+// document.getElementById('contact').onclick = changePage(pageName)
+// }
