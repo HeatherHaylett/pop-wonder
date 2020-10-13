1159391291eda816eede887ea93f58d730177dec
Heather Haylett <heatherhaylett@gmail.com>
2020-10-13 16:21:26 -0500

    (feat) create event data

---
+++ events.js
@@ -0,0 +1,14 @@
+export const events = [
+    {
+        name: 'Farmer\'s Market',
+        date: 'Saturday, October 17th'
+    },
+    {
+        name: 'Art Walk',
+        date: 'Tuesday, October 20th'
+    },
+    {
+        name: 'Farmer\'s Market',
+        date: 'Saturday, October 24th'
+    }
+]
