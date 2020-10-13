f37c3d12d3bd6a97dba342e8dece1c0eca2235bc
Heather Haylett <heatherhaylett@gmail.com>
2020-10-13 16:21:15 -0500

    (feat) render events

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+import { events } from './events.js';
 
 
+const eventSelector = document.querySelector('.events');
+eventSelector.innerHTML = `
+    <p class="event-name">${events[0].name}</p>
+    <p class="event-date">${events[0].date}</P>
+    <p class="event-name">${events[1].name}</p>
+    <p class="event-date">${events[1].date}</P>
+    <p class="event-name">${events[2].name}</p>
+    <p class="event-date">${events[2].date}</P>
+    `;
 
-// const changePage = (pageName) => {
-//     window.location.href = `${pageName}.html`;
-// }
 
-// const clickEvent = (pageName) => {
-// document.getElementById('contact').onclick = changePage(pageName)
-// }
